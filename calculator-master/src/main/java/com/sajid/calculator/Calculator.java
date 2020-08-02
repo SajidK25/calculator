95620ae4dd940cf20e2acc472fd2e957bdeb3a35
Sajid Khan <sajidk25@gmail.com>
2020-08-02 20:18:02 +0600

    stage package, docker build and push added

---
+++ calculator-master/src/main/java/com/sajid/calculator/Calculator.java
@@ -0,0 +1,13 @@
+package com.sajid.calculator;
+import org.springframework.stereotype.Service;
+
+@Service
+public class Calculator {
+     int sum(int a, int b) {
+          return a + b;
+     }
+     
+     int subtract(int a, int b) {
+          return a - b;
+     }
+}
